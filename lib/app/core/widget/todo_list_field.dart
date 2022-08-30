a2182a75a321380a69c3a3745c0a90d01bbfec7a
Douglas Ceron <ceron.douglas@gmail.com>
2022-08-30 15:45:22 -0300

    custom textFormField

---
+++ lib/app/core/widget/todo_list_field.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
+
+class TodoListField extends StatelessWidget {
+  final String label;
+  final bool showEyeIcon;
+  final listenNotifier = ValueNotifier(false);
+  final TextEditingController? controller;
+  final FormFieldValidator<String>? validator;
+
+  bool obscureText;
+
+  TodoListField({
+    Key? key,
+    required this.label,
+    this.showEyeIcon = false,
+    this.obscureText = false,
+    this.controller,
+    this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: listenNotifier,
+      builder: (_, isPasswordVisible, __) {
+        return TextFormField(
+          controller: controller,
+          validator: validator,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            isDense: true,
+            labelText: label,
+            labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+            suffixIcon: showEyeIcon
+                ? IconButton(
+                    onPressed: () {
+                      listenNotifier.value = !isPasswordVisible;
+                      obscureText = !obscureText;
+                    },
+                    icon: Icon(
+                      isPasswordVisible
+                          ? TodoListIcons.eye
+                          : TodoListIcons.eyeSlash,
+                      size: 15,
+                    ),
+                  )
+                : null,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(50),
+              borderSide: const BorderSide(
+                color: Colors.red,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
 import 'package:path/path.dart';
+import 'package:todo_list_provider/app/core/widget/todo_list_field.dart';
 import 'package:todo_list_provider/app/core/widget/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
@@ -23,16 +24,22 @@ class LoginPage extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     const SizedBox(height: 10),
-                    TodoListLogo(),
+                    const TodoListLogo(),
                     Padding(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 40.0, vertical: 20),
                       child: Form(
                         child: Column(
                           children: [
-                            TextFormField(),
+                            TodoListField(
+                              label: 'E-mail',
+                            ),
                             const SizedBox(height: 20),
-                            TextFormField(),
+                            TodoListField(
+                              label: 'Password',
+                              obscureText: true,
+                              showEyeIcon: true,
+                            ),
                             const SizedBox(height: 10),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -42,7 +49,7 @@ class LoginPage extends StatelessWidget {
                                     child: const Text('Esqueceu sua senha?')),
                                 ElevatedButton(
                                   onPressed: () {},
-                                  child: const Padding(
+                                  child: Padding(
                                     padding: EdgeInsets.all(12.0),
                                     child: Text('Login'),
                                   ),
@@ -58,32 +65,46 @@ class LoginPage extends StatelessWidget {
                         ),
                       ),
                     ),
-                    const SizedBox(height: 30),
-                    Divider(
-                      color: Colors.grey.withAlpha(50),
-                      thickness: 2,
-                    ),
-                    SignInButton(
-                      Buttons.Google,
-                      onPressed: () {},
-                      padding: EdgeInsets.all(5),
-                      shape: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      text: 'Continue com o Google',
-                    ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Não tem conta?'),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text('Cadastre-se'),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            top: BorderSide(
+                              width: 2,
+                              color: Colors.grey.withAlpha(50),
+                            ),
+                          ),
+                          color: const Color(0xffF0F3F7),
                         ),
-                      ],
-                    )
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 30),
+                            SignInButton(
+                              Buttons.Google,
+                              onPressed: () {},
+                              padding: const EdgeInsets.all(5),
+                              shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none,
+                              ),
+                              text: 'Continue com o Google',
+                            ),
+                            const SizedBox(height: 15),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Não tem conta?'),
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text('Cadastre-se'),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
